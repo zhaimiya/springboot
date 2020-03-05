1f09bfb3c7145e612e030b85554f5c4f80527b26
zhaimiya <zhaimiya@gmail.com>
2020-03-05 18:10:44 +0800

    mybatis-generator

--- web/src/main/java/com/example/SpringbootApplication.java
+++ web/src/main/java/com/example/SpringbootApplication.java
@@ -1,16 +1,25 @@
 package com.example;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class,
+        scanBasePackages = {"com.example"})
+@MapperScan(value = {"com.example.dao"})
 public class SpringbootApplication {
+
 
     public static void main(String[] args) {
         // 完全禁用重启
-       //System.setProperty("spring.devtools.restart.enabled", "false");
+        System.out.println(" -- hello --");
+        //System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(SpringbootApplication.class, args);
-        System.out.printf("hello world");
+        System.out.printf(" -- world --");
     }
 
 }
---
+++ web/src/main/java/com/example/config/MyApplicationRunner.java
@@ -0,0 +1,23 @@
+package com.example.config;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+
+/**
+ * 项目启动时候运行
+ */
+@Component
+public class MyApplicationRunner implements ApplicationRunner, Ordered {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("ApplicationRunner.run()-- -- ----");
+    }
+
+
+    @Override
+    public int getOrder() {
+        return 10;
+    }
+}
